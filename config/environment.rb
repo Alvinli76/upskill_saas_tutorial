6e8760cdfa9625b3135f5ac8bad3f6434e35eb27
Ze Li <Zeliop72@gmail.com>
2017-02-10 05:01:55 +0000

    added code comments

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'warm-citadel-39734.herokuapp.com',
+  :domain         => 'warm-citadel-39734.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
