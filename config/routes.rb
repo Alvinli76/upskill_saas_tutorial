58aad4f231300a4dcf43e2d044a7fe5e649c0119
Ze Li <Zeliop72@gmail.com>
2017-02-12 20:29:09 +0000

    blah blah blah im stuck

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+    
     root to: 'pages#home'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'about', to: 'pages#about'
     get 'faq', to: 'pages#faq'
     resources :contacts, only: :create
